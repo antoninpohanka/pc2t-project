16e540e9e09b4524a04b559f68a7d9f0c68ac112
JanaLangovaTLI <270635@vut.cz>
2026-05-05 21:19:22 +0200

    smazani komentu

--- DB_Zamestnanci/src/db/BezpSp.java
+++ DB_Zamestnanci/src/db/BezpSp.java
@@ -11,39 +11,6 @@ public class BezpSp extends Zamestnanec {
 
 	}
 
-	/*@Override
-	public void dovednost() {
-		// nepotrebuje parametr,
-		// podobny algoritmus u bezpsp ale aplikovany na celou db
-		/*float prumSpol = 0;
-
-		// mozna misto tohodle pouzit metodu z DB co pocita prumernou spolupraci?
-		for (UrovSpol u : this.getListZam().values()) {
-			prumSpol += u.ordinal() + 1;
-		}
-
-		prumSpol = prumSpol / this.getListZam().size();
-
-		System.out.println("Prumerna spoluprace je: " + prumSpol);
-
-		// definujeme si napr. rizik skore - 1 az 10
-
-		prumSpol = 10 - (prumSpol * (10 / 3));
-
-		String riziko;
-
-		if (prumSpol < 4) {
-			riziko = "nizke";
-		} else if (prumSpol >= 4 || prumSpol <= 6) {
-			riziko = "stredni";
-		} else {
-			riziko = "vysoke";
-		}
-
-		System.out.println("Riziko spoluprace je: " + prumSpol + " - " + riziko);
-		
-	} */
-
 	@Override
 	public String toString() {
 		return String.format("[ID: %d] %s %s (nar. %d) - Bezpecnostni specialista", ID, jmeno, prijm, rokNaroz);
